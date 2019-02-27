d45c1afa8e9f9e43dadc20a13bdaf10a68f276e4
Kevin Simpson <relasine@gmail.com>
2019-02-27 09:58:11 -0700

    Add signup and reset password fetch call logic to fetchcalls.js

--- src/utilities/fetchcalls.js
+++ src/utilities/fetchcalls.js
@@ -15,3 +15,39 @@ export const callLogin = async payload => {
 
   return await response.json();
 };
+
+export const callSignup = async payload => {
+  const requestObject = {
+    method: "POST",
+    mode: "cors",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  const response = await fetch(
+    "http://localhost:3000/teachers/signup",
+    requestObject
+  );
+
+  return await response.json();
+};
+
+export const callResetPassword = async payload => {
+  const requestObject = {
+    method: "POST",
+    mode: "cors",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  const response = await fetch(
+    "http://localhost:3000/teachers/resetpassword",
+    requestObject
+  );
+
+  return await response.json();
+};
